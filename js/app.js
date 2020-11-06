d1775596092c7280349573c107f53989bc5e9d9b
Sławomir Brzozowski <brzozasr@gmail.com>
2020-11-06 14:45:24 +0100

    Add removePicture function

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@
 
     let shufflePicturesName = shuffle([0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5]);
 
+    let picturesDiscovered = [];
+
     function shuffle(array) {
         // copy of the "array"
         let tmpArray = array.slice(0, array.length);
@@ -31,10 +33,16 @@
             selectedDiv.style.transform = "rotatey(" + k + "deg)";
             selectedDiv.style.transitionDuration = "0.5s";
             selectedDiv.appendChild(newImg);
+            picturesDiscovered.push(newImg.getAttribute("src"));
 
-            if (countDiscovered() === 2) {
-                console.log(countDiscovered());
-                setTimeout(hidePictures, 1200);
+            if (countDiscovered() === 2 && picturesDiscovered.length === 2) {
+                if (picturesDiscovered[0] === picturesDiscovered[1]) {
+                    setTimeout(removePictures, 1200);
+                } else {
+                    setTimeout(hidePictures, 1200);
+                }
+
+                picturesDiscovered.splice(0, picturesDiscovered.length);
             }
         }
     }
@@ -50,12 +58,26 @@
                 newImg.setAttribute('alt', 'Image hide');
                 image.innerHTML = "";
                 image.appendChild(newImg);
+                image.removeAttribute('style');
             }
         });
     }
 
     function removePictures() {
+        pictures.forEach(image => {
+            let images = image.getElementsByTagName('img');
+            let imgSrc = images.item(0).getAttribute('src');
 
+            if (!imgSrc.endsWith("back.png") && !imgSrc.endsWith("empty.png")) {
+                let newImg = document.createElement('img');
+                newImg.src = "img/empty.png";
+                newImg.setAttribute('alt', 'Empty image');
+                image.innerHTML = "";
+                image.appendChild(newImg);
+                image.removeAttribute('style');
+                image.removeEventListener("click", showPicture)
+            }
+        });
     }
 
     function countDiscovered() {
